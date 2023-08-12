9c01504e7624fbae1c5c4343e8e35a74a8c41196
shkoniku <113796273+shkoniku@users.noreply.github.com>
2023-08-12 23:59:12 +0300

    Add xml parser and making sql bd

--- qt/server.cpp
+++ qt/server.cpp
@@ -10,11 +10,45 @@ Server::Server()
     {
         qDebug() << "error";
     }
+    QSqlDatabase sqlitedb = QSqlDatabase::addDatabase("QSQLITE");
+    sqlitedb.setDatabaseName("../qt/blocks.sqlite");
+    if (!sqlitedb.open())
+    {
+        qDebug() << "error opening database";
+    }
+    QString queryTable = "CREATE TABLE blocks("
+                    "id TEXT PRIMARY KEY, "
+                    "Name TEXT, "
+                    "IP TEXT, "
+                    "BoardCount TEXT, "
+                    "MtR TEXT, "
+                    "MtC TEXT, "
+                    "Description TEXT, "
+                    "Label TEXT);";
+    QSqlQuery query;
+    if (!query.exec(queryTable)) qDebug() << "error creating table: blocks";
+    queryTable = "CREATE TABLE boards("
+                 "blockId TEXT, "
+                 "id TEXT PRIMARY KEY, "
+                 "Num TEXT, "
+                 "Name TEXT, "
+                 "PortCount TEXT, "
+                 "IntLinks TEXT, "
+                 "Algoritms TEXT);";
+    if (!query.exec(queryTable)) qDebug() << "error creating table: boards";
+    queryTable = "CREATE TABLE ports("
+                 "boardId TEXT, "
+                 "id TEXT PRIMARY KEY, "
+                 "Num TEXT, "
+                 "Media TEXT, "
+                 "Signal TEXT);";
+    if (!query.exec(queryTable)) qDebug() << "error creating table: ports";
     QDir directory("../qt/XMLS");
     QStringList files = directory.entryList(QStringList() << "*.xml", QDir::Files);
     foreach (QString nameFile, files) {
         QFile* file = new QFile("../qt/XMLS/"+nameFile);
         qDebug() << file->fileName();
+        QString blockId = "0", boardId = "0";
         if (file->open(QIODevice::ReadOnly | QIODevice::Text) && QFileInfo::exists("../qt/XMLS/"+nameFile) && QFileInfo("../qt/XMLS/"+nameFile).isFile())
         {
             qDebug() << "ok";
@@ -28,7 +62,48 @@ Server::Server()
                 }
                 if (token == QXmlStreamReader::StartElement)
                 {
-                    //if (xml.name() == "")
+
+                    if (xml.name() == "block")
+                    {
+                        QString attributs[8] = {NULL};
+                        for (int iterator = 0; iterator < xml.attributes().size(); iterator++)
+                        {
+                            if (iterator == 0)
+                            {
+                                blockId = xml.attributes()[0].value().toString();
+                            }
+                            attributs[iterator] = xml.attributes()[iterator].value().toString();
+                        }
+                        InsertDataToBlocks(attributs[0], attributs[1], attributs[2],
+                                            attributs[3], attributs[4], attributs[5],
+                                            attributs[6], attributs[7]);
+                    }
+
+                    if (xml.name() == "board")
+                    {
+                        QString attributs[6] = {NULL};
+                        for (int iterator = 0; iterator < xml.attributes().size(); iterator++)
+                        {
+                            if (iterator == 0)
+                            {
+                                boardId = xml.attributes()[0].value().toString();
+                            }
+                            attributs[iterator] = xml.attributes()[iterator].value().toString();
+                        }
+                        InsertDataToBoards(blockId, attributs[0], attributs[1], attributs[2],
+                                            attributs[3],  attributs[4],  attributs[5]);
+                    }
+
+                    if (xml.name() == "port")
+                    {
+                        QString attributs[4] = {NULL};
+                        for (int iterator = 0; iterator < xml.attributes().size(); iterator++)
+                        {
+                            attributs[iterator] = xml.attributes()[iterator].value().toString();
+                        }
+                        InsertDataToPorts(boardId, attributs[0], attributs[1],
+                                          attributs[2], attributs[3]);
+                    }
                 }
             }
         }
@@ -39,6 +114,77 @@ Server::Server()
         file->close();
     }
 
+}
+
+
+void Server::InsertDataToBlocks(QString &id, QString &Name, QString &IP, QString &BoardCount,
+                                QString &MtR, QString &MtC, QString &Description,
+                                QString &Label)
+{
+    QSqlQuery query;
+    query.prepare("INSERT INTO blocks("
+                  "id, "
+                  "Name, "
+                  "IP, "
+                  "BoardCount, "
+                  "MtR, "
+                  "Mtc, "
+                  "Description, "
+                  "Label) "
+                  "VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    query.addBindValue(id);
+    query.addBindValue(Name);
+    query.addBindValue(IP);
+    query.addBindValue(BoardCount);
+    query.addBindValue(MtR);
+    query.addBindValue(MtC);
+    query.addBindValue(Description);
+    query.addBindValue(Label);
+    if (!query.exec()) qDebug() << "error adding values to table: blocks id = " << id;
+
+}
+
+
+void Server::InsertDataToBoards(QString &blockId, QString &id, QString &Num, QString &Name,
+                                QString &PortCount, QString &IntLinks, QString &Algoritms)
+{
+    QSqlQuery query;
+    query.prepare("INSERT INTO boards("
+                  "blockId, "
+                  "id, "
+                  "Num, "
+                  "Name, "
+                  "PortCount, "
+                  "IntLinks, "
+                  "Algoritms) "
+                  "VALUES (?, ?, ?, ?, ?, ?, ?)");
+    query.addBindValue(blockId);
+    query.addBindValue(id);
+    query.addBindValue(Num);
+    query.addBindValue(Name);
+    query.addBindValue(PortCount);
+    query.addBindValue(IntLinks);
+    query.addBindValue(Algoritms);
+    if (!query.exec()) qDebug() << "error adding values to table: boards. id = " << id;
+}
+
+
+void Server::InsertDataToPorts(QString &boardId, QString &id, QString &Num, QString &Media, QString &Signal)
+{
+    QSqlQuery query;
+    query.prepare("INSERT INTO ports("
+                  "boardId, "
+                  "id, "
+                  "Num, "
+                  "Media, "
+                  "Signal)"
+                  "VALUES (?, ?, ?, ?, ?)");
+    query.addBindValue(boardId);
+    query.addBindValue(id);
+    query.addBindValue(Num);
+    query.addBindValue(Media);
+    query.addBindValue(Signal);
+    if (!query.exec()) qDebug() << "error adding values to table: ports. id = " << id;
 }
 
 
